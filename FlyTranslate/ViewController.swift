7b3d9caa8426f24163d68800f39c31a8c03db23b
Denis Garifyanov <daneegar@gmail.com>
2018-11-29 20:33:32 +0500

    Initial Commit

---
+++ FlyTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlyTranslate
+//
+//  Created by Denis Garifyanov on 29/11/2018.
+//  Copyright © 2018 Denis Garifyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
